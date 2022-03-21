3f8fd8a76fcb2db54e7e6050e089d376a573dbda
luiz <lbrebelatto@gmail.com>
2022-03-21 00:44:45 -0300

    style: criação do estilo global e aplicação no arquivo principal :lipstick:
    adição das cores e fontes utilizadas no app

---
+++ src/global/styles/theme.ts
@@ -0,0 +1,16 @@
+export default {
+  colors: {
+    pink_dark: '#B73058',
+    pink_light: '#E06C88',
+    grey: '#8D8D8D',
+    grey_dark: '#2F394B',
+    grey_light: '#9CABC2',
+    text: '#E7DFDD'
+  },
+  
+  fonts: {
+    regular: 'Poppins_400Regular',
+    medium: 'Poppins_500Medium',
+    bold: 'Poppins_700Bold',
+  }
+}
