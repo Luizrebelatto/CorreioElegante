00381a8f52ab4b67063821115f28eb8b0b2c31f1
luiz <lbrebelatto@gmail.com>
2022-03-21 00:57:08 -0300

    styles: aplicação das fontes globais :art:
    implementação da função que carrega as fontes e importação dos nomes das fontes

--- App.tsx
+++ App.tsx
@@ -1,10 +1,23 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import AppLoading from 'expo-app-loading';
 import { ThemeProvider } from 'styled-components';
+
+import { useFonts, Poppins_400Regular, Poppins_500Medium, Poppins_700Bold} from '@expo-google-fonts/poppins';
 
 import theme from './src/global/styles/theme';
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Poppins_400Regular,
+    Poppins_500Medium,
+    Poppins_700Bold
+  })
+
+  if(!fontsLoaded){
+    return <AppLoading/>
+  }
+  
   return (
     <ThemeProvider theme={theme}>
       <View style={styles.container}>
