dc816500a89639ea5f3ecdd80fcfee43e0239331
luiz <lbrebelatto@gmail.com>
2022-03-26 22:36:14 -0300

    style: criação imagem do header e aplicação gradiente  :lipstick:

--- src/screens/Home/home.styles.ts
+++ src/screens/Home/home.styles.ts
@@ -1,3 +1,17 @@
 import styled from "styled-components/native";
+import { LinearGradient } from 'expo-linear-gradient';
 
-export const Title = styled.Text``;
+export const Circle = styled(LinearGradient)`
+  width: 330px;
+  height: 330px;
+  background-color: #fff;
+  border-radius: 164px;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const Image = styled.Image`
+  width: 227px;
+  height: 252px;
+  margin-left: 20px;
+`;
--- src/screens/Home/home.tsx
+++ src/screens/Home/home.tsx
@@ -1,12 +1,24 @@
 import React from 'react';
 
+import ImageSticker from '../../assets/png/GroupDiningTable.png';
 import { Container } from '../../shared/generals'
-import { Title } from './home.styles';
+import { Circle, Image } from './home.styles';
+
+import { View } from 'react-native';
 
 export default function Home(){
   return(
     <Container>
-      <Title>Home</Title>
+      <View style={{alignItems:'center', marginTop: 30}}>
+      <Circle
+        colors={['#C4C4C4','#FFFFFF']}
+        start={{x: 0, y: 0}}
+        end={{x: 1, y: 1}}
+      >
+        <Image source={ImageSticker}/>
+      </Circle>
+
+      </View>
     </Container>
   )
 }
